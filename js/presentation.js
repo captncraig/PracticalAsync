6fbcb6a4eca20cf9a3317fdfa2b440163180e1aa
Craig Peterson <cpeterson@ancestry.com>
2013-03-05 10:47:16 -0700

    A bunch of work.
    Worst commit message ever.

--- js/presentation.js
+++ js/presentation.js
@@ -1,13 +1,14 @@
 document.getElementById("impress").addEventListener( "impress:stepenter", function (event) {
 
     var step = event.target;
-
+	var api = impress();
 	if(step.id==='concurrency'){
-		$(".remain").removeClass('remain');
+		$(".word").removeClass('remain');
 		$("#conc").css('opacity','1');
+		api.next();
 		$("#conc").animate({"font-size":"600%"},1000);
 	}
-	if(step.id==='clone'){
+	if(step.id==='synch'){
 		$("#conc").css('opacity','0');
 	}
 
