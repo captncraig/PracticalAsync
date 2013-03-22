b5e63a53bd8db49952af55a707020a86588e3d21
Craig Peterson <peterson.craig@gmail.com>
2013-03-22 15:53:30 -0600

    Awesome TCS based test.

--- UnitTests/UnitControllerTests.cs
+++ UnitTests/UnitControllerTests.cs
@@ -24,12 +24,45 @@ namespace UnitTests
         }
 
         [Test]
+        ///Basic Test of asynchronous method. Mocking is easy, just return a task.
+        /// Using Task.FromResult to make essentially a noop task.
         public async void GetALlUnits()
         {
             IList<UnitSummary> list = new List<UnitSummary>();
             _db.Setup(x => x.GetAllUnits()).Returns(Task.FromResult(list));
             var result = await _controller.GetAllUnits();
             Assert.That(result, Is.SameAs(list));
+        }
+
+        [Test]
+        public void GetUnit()
+        {
+            var unitTcs = new TaskCompletionSource<Unit>();
+            var buildingTcs = new TaskCompletionSource<string>();
+            var countersTcs = new TaskCompletionSource<IList<string>>();
+
+            //Setup mocks to return tasks from completionsources
+            _db.Setup(x => x.GetUnit("marine")).Returns(unitTcs.Task);
+            _webService.Setup(x => x.BuiltFrom("marine")).Returns(buildingTcs.Task);
+            _webService.Setup(x => x.GetCountersFor("marine")).Returns(countersTcs.Task);
+
+            //call controller method. Should not be complete, but all three subTasks should have started
+            var result = _controller.GetUnit("marine");
+            Assert.That(result.IsCompleted, Is.False);
+            _db.VerifyAll();
+            _webService.VerifyAll();
+
+            //Nothing has finished yet. Complete tasks in any order
+            var unit = new Unit();
+            unitTcs.SetResult(unit);
+            buildingTcs.SetResult("barracks");
+            countersTcs.SetResult(new List<string>{"foo","bar"});
+            
+            //Nw we should be finished.
+            Assert.That(result.IsCompleted);
+            Assert.That(result.Result, Is.SameAs(unit));
+            Assert.That(unit.BuiltFrom, Is.EqualTo("barracks"));
+
         }
     }
 }
